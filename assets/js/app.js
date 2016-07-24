1397e3b33a5c1b7e533a46d614bf3e7be7a6dda2
Pushpa <psmyid@gmail.com>
2016-07-24 18:14:56 +0530

    angular sample page

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', ['ui.bootstrap']);
+
+myApp.controller('sidebarCtrl', function ($scope) {
+
+});
+
+myApp.controller('modalCtrl', function ($scope, $uibModal, $log) {
+  
+});
